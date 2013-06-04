6549d021132aa0edaac35a0bc007ad927d04be91
root <root@erpserver.(none)>
2013-06-04 12:44:07 +0200

    Add csrf support for controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   after_filter :set_access_control_headers
 
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://corthay.self.local'
+    headers['Access-Control-Allow-Origin'] = 'http://erp.corthay.self.local'
     headers['Access-Control-Request-Method'] = 'POST, GET, PUT, DELETE'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, X-CSRF-Token,  Accept'
     headers['Access-Control-Allow-Credentials'] = 'true'
   end
 end
